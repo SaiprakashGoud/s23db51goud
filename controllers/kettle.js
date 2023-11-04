b50c86c9e753e1370aa16b5541ec7e14dd5a9da0
SaiprakashGoud <143534112+SaiprakashGoud@users.noreply.github.com>
2023-11-04 01:26:10 -0500

    Created kettles.js in directories--models and controllers.updated code in app.js

--- controllers/kettle.js
+++ controllers/kettle.js
@@ -1,73 +1,21 @@
 var Kettle = require('../models/kettle');
-        // List of all Kettles
-        exports.kettle_list = async function(req, res) {
-                try{
-                theKettle= await Kettle.find();
-                res.send(theKettle);
-                }
-                catch(err){
-                res.status(500);
-                res.send(`{"Error is ": ${err}}`);
-                }
-            };
-
-        // for a specific Kettle.
-        exports.kettle_detail = async function(req, res) {
-            console.log("detail" + req.params.id)
-            try {
-            result = await Kettle.findById( req.params.id)
-            res.send(result)
-            }
-            catch (err) {
-            res.status(500)
-            res.send(`{"error": document for id ${req.params.id} not found`);
-            }
-           };
-        // Handle Kettle create on POST.
-        exports.kettle_create_post = async function(req, res) {
-            console.log(req.body)
-            let document = new Kettle();
-            document.kettle_name = req.body.kettle_name;
-            document.quantity = req.body.quantity;
-            document.resistance = req.body.resistance;
-            try{
-            let result = await document.save();
-            res.send(result);
-            }
-            catch(err){
-            res.status(500);
-            res.send(`{"error is ": ${err}}`);
-            }
-           };
-        // Handle Kettle delete form on DELETE.
-        exports.kettle_delete = async function(req, res) {
-            console.log("delete " + req.params.id)
-            try {
-            result = await Kettle.findByIdAndDelete( req.params.id)
-            console.log("Removed " + result)
-            res.send(result)
-            }
-            catch (err) {
-            res.status(500)
-            res.send(`{"error": Error deleting ${err}}`);
-            }
-        };
-        // Handle Kettle update form on PUT.
-        exports.kettle_update_put = async function(req, res) {
-            console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
-            try {
-            let toUpdate = await Kettle.findById( req.params.id)
-            // Do updates of properties
-            if(req.body.kettle_name)
-            toUpdate.kettle_name = req.body.kettle_name;
-            if(req.body.quantity) toUpdate.quantity = req.body.quantity;
-            if(req.body.resistance) toUpdate.resistance = req.body.resistance;
-            let result = await toUpdate.save();
-            console.log("Sucess " + result)
-            res.send(result)
-            } catch (err) {
-            res.status(500)
-            res.send(`{"error": ${err}: Update for id ${req.params.id}
-        failed`);
-            }
-        };
+// List of all kettles
+exports.kettle_list = function(req, res) {
+res.send('NOT IMPLEMENTED: kettle list');
+};
+// for a specific kettle.
+exports.kettle_detail = function(req, res) {
+res.send('NOT IMPLEMENTED: kettle detail: ' + req.params.id);
+};
+// Handle kettle create on POST.
+exports.kettle_create_post = function(req, res) {
+res.send('NOT IMPLEMENTED: kettle create POST');
+};
+// Handle kettle delete form on DELETE.
+exports.kettle_delete = function(req, res) {
+res.send('NOT IMPLEMENTED: kettle delete DELETE ' + req.params.id);
+};
+// Handle kettle update form on PUT.
+exports.kettle_update_put = function(req, res) {
+res.send('NOT IMPLEMENTED: kettle update PUT' + req.params.id);
+};
