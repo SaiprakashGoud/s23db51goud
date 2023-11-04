24cbb269a719ea6a95e0fe303baef671e4e98280
SaiprakashGoud <143534112+SaiprakashGoud@users.noreply.github.com>
2023-11-04 04:07:36 -0500

    implemented create_post method

--- controllers/kettle.js
+++ controllers/kettle.js
@@ -37,3 +37,23 @@ exports.kettle_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
     };
+
+    exports.kettle_create_post = async function(req, res) {
+        console.log(req.body)
+        let document = new Kettle();
+        // We are looking for a body, since POST does not have query parameters.
+        // Even though bodies can be in many different formats, we will be picky
+        // and require that it be a json object
+        // {"costume_type":"goat", "cost":12, "size":"large"}
+        document.kettle_name = req.body.kettle_name;
+        document.quantity = req.body.quantity;
+        document.resistance = req.body.resistance;
+        try{
+        let result = await document.save();
+        res.send(result);
+        }
+        catch(err){
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+        }
+        };
