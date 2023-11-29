05de4f737916902321537070d98a5569e7954ff9
SaiprakashGoud <143534112+SaiprakashGoud@users.noreply.github.com>
2023-11-29 11:11:58 -0600

    added validator to kettle schema

--- models/kettle.js
+++ models/kettle.js
@@ -6,7 +6,16 @@ const kettleSchema = mongoose.Schema({
     },
     quantity: {
         type:Number,
-        required:true
+        required:true,
+        min:[1,"quantity is greater than one"],
+        max:[100,"quantity must be less than 100 "],
+        validate:{
+            validator:function(value){
+                return "Quantity must be between 1 to 100 and no negatives values "
+            },
+            message: error1 => `${error1.value} is not within the limit`
+        }
+    
     },
     resistance: {
         type: String,
